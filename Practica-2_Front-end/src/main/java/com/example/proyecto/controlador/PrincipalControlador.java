1a4f7f607fbffb8fb924cd7cde026257f154824d
armando122 <armandoramirezg@ciencias.unam.mx>
2022-05-26 19:00:40 -0500

    Primeros cambios

--- Practica-2_Front-end/src/main/java/com/example/proyecto/controlador/PrincipalControlador.java
+++ Practica-2_Front-end/src/main/java/com/example/proyecto/controlador/PrincipalControlador.java
@@ -1,8 +1,6 @@
 package com.example.proyecto.controlador;
 
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.Cookie;
@@ -12,34 +10,10 @@ import java.security.Principal;
 
 @Controller
 public class PrincipalControlador {
+
     @RequestMapping("/")
-    public String index(Model model, String error, Principal principal) {
-
-        if (error != null) {
-            model.addAttribute("error", true);
-        }
-
-        if (principal != null) {
-            return "redirect:/notas/";
-        }
-        return "index";
-    }
-
-    @RequestMapping("/registro")
-    public String registro() { return "registro"; }
-
-    @RequestMapping("/salir")
-    public String salir(HttpServletRequest request){
-        HttpSession session = request.getSession(false);
-        SecurityContextHolder.clearContext();
-        session = request.getSession(false);
-        if (session != null) {
-            session.invalidate();
-        }
-        for (Cookie cookie : request.getCookies()) {
-            cookie.setMaxAge(0);
-        }
-        return "redirect:/";
+    public String index() {
+        return "nueva-nota";
     }
 
 }
